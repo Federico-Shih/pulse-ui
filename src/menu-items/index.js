a4699d8b27fc18b4792450c3b15a1df232911e56
Federico <fedeshih@gmail.com>
2023-03-31 23:22:38 -0300

    chore: created gitignore

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -1,12 +1,9 @@
 import dashboard from './dashboard';
-import pages from './pages';
-import utilities from './utilities';
-import other from './other';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-    items: [dashboard, pages, utilities, other]
+    items: [dashboard]
 };
 
 export default menuItems;
