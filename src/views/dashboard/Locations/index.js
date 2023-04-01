97f01d0cee1f6ee092146c30ce17829c0ca63450
Ignacio R.J <38332696+Nachrj@users.noreply.github.com>
2023-04-01 05:06:59 -0300

    locations displayed

--- src/views/dashboard/Locations/index.js
+++ src/views/dashboard/Locations/index.js
@@ -1,14 +1,55 @@
 import { useEffect, useState } from 'react';
 
 // material-ui
-import { Grid, Typography, IconButton, Stack } from '@mui/material';
+import { Grid, Typography, IconButton, Stack, Card, CardContent } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import { useTheme } from '@mui/material/styles';
 
 // project imports
 import { gridSpacing } from 'store/constant';
 import CreateModal from 'ui-component/models/CreateModal.js';
 
 // ==============================|| DEFAULT DASHBOARD ||============================== //
+
+const MOCKED_LOCATIONS = [
+    {
+        title: "Kitchen",
+        description: "This is the kitchen"
+    },
+    {
+        title: "Living Room",
+        description: "This is the living room"
+    },
+    {
+        title: "Bedroom",
+        description: "This is the bedroom"
+    },
+]
+
+function LocationCard({ title, description }) {
+    const theme = useTheme();
+
+    return ( 
+        <Card                 
+            sx={{
+                borderColor: theme.palette.primary[200] + 25,
+                ':hover': {
+                    boxShadow: theme.shadows[16]
+                },
+                width: "25vh",
+                height: "20vh",
+        }}>
+            <CardContent>
+                <Typography variant="h4" pb={1}>
+                    {title}
+                </Typography>
+                <Typography >
+                    {description}
+                </Typography>    
+            </CardContent>
+        </Card>
+    )
+}
 
 const Locations = () => {
     const [isLoading, setLoading] = useState(true);
@@ -33,6 +74,13 @@ const Locations = () => {
                             </IconButton>
                             <CreateModal open={open} handleClose={handleClose} type="Locations" />
                         </Stack>
+                    </Grid>
+                    <Grid item container xs={12} justifyContent="flex-start">
+                        {MOCKED_LOCATIONS.map((location) => (
+                            <Grid item xs={3}> 
+                                <LocationCard title={location.title} description={location.description} />
+                            </Grid>
+                        ))}
                     </Grid>
                 </Grid>
             </Grid>
